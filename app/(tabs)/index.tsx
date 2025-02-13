d1a10cb9735225956a304a6533a7833596005308
xebec19 <rohant946@gmail.com>
2025-02-13 22:47:33 +0530

    feat: hero image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { View, StyleSheet } from "react-native";
+
+const PLACE_HOLDER = require("@/assets/images/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Screen</Text>
-      <Link href="/about">About</Link>
+      <View style={styles.imageContainer}>
+        <Image source={PLACE_HOLDER} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -15,9 +18,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
   text: {
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 10,
   },
 });
