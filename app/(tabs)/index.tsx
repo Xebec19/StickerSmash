a7a37f859f986b321bede9c0df866e6f9f47d07c
xebec19 <rohant946@gmail.com>
2025-02-19 21:32:15 +0530

    index screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
 import { Image } from "expo-image";
 import { View, StyleSheet } from "react-native";
 
@@ -7,7 +9,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PLACE_HOLDER} style={styles.image} />
+        <ImageViewer imgSource={PLACE_HOLDER} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a Photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -29,5 +35,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 10,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
